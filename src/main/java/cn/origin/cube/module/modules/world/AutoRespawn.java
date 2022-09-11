bf01bf1698e284306393799577f67b74a7276e9a
jiyun233 <superstupid233@163.com>
2022-09-11 23:50:47 +0800

    fix auto respawn

--- src/main/java/cn/origin/cube/module/modules/world/AutoRespawn.java
+++ src/main/java/cn/origin/cube/module/modules/world/AutoRespawn.java
@@ -1,11 +1,9 @@
 package cn.origin.cube.module.modules.world;
 
-import cn.origin.cube.event.events.client.DisplayGuiScreenEvent;
 import cn.origin.cube.module.Category;
 import cn.origin.cube.module.Module;
 import cn.origin.cube.module.ModuleInfo;
 import net.minecraft.client.gui.GuiGameOver;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @ModuleInfo(name = "AutoRespawn", descriptions = "Anti Death Screen", category = Category.WORLD)
 public class AutoRespawn extends Module {
